b05d389456b8c063d2e8f8d6a0926c51b1a42a70
bUzR4Ks <aulia.bilibili@gmail.com>
2025-03-14 08:12:57 +0700

    initial code

--- laravel-profile-app/app/Http/Controllers/ProfileController.php
+++ laravel-profile-app/app/Http/Controllers/ProfileController.php
@@ -1,16 +1,15 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function submit(Request $request)
+    public function submit(Request $request) 
     {
-        // Validate and store the file
-        $path = $request->file('profile_photo')->store('public/profile_photos');
+        $path = $request->file('profile_photo')->store('profile_photo', 'public');
 
-        // Gather data to pass to the view
         $data = $request->only(['name', 'email']);
         $data['photo_path'] = $path;
 
--- laravel-profile-app/resources/views/profile_result.blade.php
+++ laravel-profile-app/resources/views/profile_result.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Result</title>
-    <!-- Bootstrap CSS for styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -15,7 +13,7 @@
         <p>Name: {{ $data['name'] }}</p>
         <p>Email: {{ $data['email'] }}</p>
         <p>Photo:</p>
-        <img src="{{ asset(str_replace('public', 'storage', $data['photo_path'])) }}" width="200" alt="Profile Photo">
+        <img src="{{ asset('storage/' . $data['photo_path']) }}" width="200" alt="Profile Photo">
     </div>
 </body>
 </html>
